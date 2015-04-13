e2dacd9c535a47f7b14a1fc9dbfa3f57ea6d0336
Brian Kropff & Patrick Fiorentino <student@epicodus.com>
2015-04-13 16:23:00 -0700

    Add star button to mark when complete

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,17 @@ $(document).ready(function() {
       task: inputtedTask
     };
 
-    $("ul#list").append("<li><span class='task'>" + newTask.task + "</span></li>" );
+    $("ol#list").append("<li><span class='task'>" + newTask.task + "</span>  " +
+    "<button type='sumbit' class='btn btn-xs'><span class='glyphicon glyphicon-star-empty'></span></button></li>");
+
 
     $("input#new_list_item").val("");
+
 
     $(".task").last().click(function() {
       $("#show-task").show();
       $("show-task h2").text(newTask.task);
+
 
       $(".task").text(newTask.task);
     });
